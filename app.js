115fca6cd53b343d7b2f1d4bb892bf4bd2a9ff7f
Playpaper <heartcolorful@gmail.com>
2023-04-05 13:40:32 +0800

    feat: use mongoose connect to mongodb

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const express = require('express')
+
+// set database connection
+const mongoose = require('mongoose')
+
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', () => console.log('mongodb error !'))
+db.once('open', () => console.log('mongodb connected !'))
+
 const app = express()
 
 const PORT = 3000
