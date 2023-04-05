085d317b1bbf91d2b35c325ca5152d7cfa24541c
Playpaper <heartcolorful@gmail.com>
2023-04-05 14:10:46 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
-
-// set database connection
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 
 if(process.env.NODE_ENV !== 'production') {
@@ -18,11 +17,17 @@ db.on('error', () => console.log('mongodb error !'))
 db.once('open', () => console.log('mongodb connected !'))
 
 const app = express()
-
 const PORT = 3000
 
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+
+
 app.get('/', (req, res) => {
-  res.send(`Hello, it's an url shortener app`)
+  res.render('index')
 })
 
 app.listen(PORT, () => {
